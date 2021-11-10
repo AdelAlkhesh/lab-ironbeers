87614bea86a1f4efbd376881b9b2511a0aeea3cf
AdelAlkhesh <splinteredsanity@outlook.com>
2021-11-10 18:16:11 +0100

    lab complete

--- app.js
+++ app.js
@@ -14,12 +14,48 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beer => {
+      res.render('beers', { beer });
+    })
+    .catch(err => {
+      res.send('404 beers not found');
+    });
+});
+
+app.get('/randombeer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beer => {
+      // let {food_pairing} = beer[0].food_pairing
+      res.render('randombeer', { beer });
+    })
+    .catch(err => {
+      res.send('404 beers not found');
+    });
+});
+
+app.get('/beers/:id', (req, res) => {
+  let { id } = req.params;
+  punkAPI
+    .getBeer(id)
+    .then(beer => {
+      res.render('beer', { beer });
+      console.log(beer);
+    })
+    .catch(err => {
+      res.send('404 beers not found');
+    });
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
